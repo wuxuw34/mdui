c021cd38864f4f7adc43b00577fcceb84c96dab8
lijianfeng <ljfwbd@gmail.com>
2026-01-18 22:05:11 +0800

    Updates

---
+++ assets/Button-DGr3DEgS.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as r}from"./index-Br6YUEzo.js";import{M as s,C as c}from"./blocks-CuTd3BXl.js";import{C as i,D as a}from"./Button.stories-hc47O4pc.js";import"./iframe-DsJvdhq0.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BtFJNINv.js";import"./button-CEfHb-QZ.js";/* empty css              */function n(t){const e={h1:"h1",p:"p",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:i}),`
+`,o.jsx(e.h1,{id:"checkbox",children:"Checkbox"}),`
+`,o.jsx(e.p,{children:"A checkbox is a square box that can be activated or deactivated when ticked."}),`
+`,o.jsx(e.p,{children:"Use checkboxes to select one or more options from a list of choices."}),`
+`,o.jsx(c,{of:a})]})}function C(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n(t)}export{C as default};
---
+++ assets/Button.stories-hc47O4pc.js
@@ -0,0 +1,21 @@
+import{M as n}from"./button-CEfHb-QZ.js";/* empty css              */const o={component:n},e={args:{children:"Default Button",variant:"default"}},r={args:{children:"Filled Button",variant:"filled"}},t={args:{children:"Outline Button",variant:"outline"}},a={args:{children:"Text Button",variant:"text"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Default Button",
+    variant: "default"
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Filled Button",
+    variant: "filled"
+  }
+}`,...r.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Outline Button",
+    variant: "outline"
+  }
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Text Button",
+    variant: "text"
+  }
+}`,...a.parameters?.docs?.source}}};const s=["Default","Filled","Outline","Text"],i=Object.freeze(Object.defineProperty({__proto__:null,Default:e,Filled:r,Outline:t,Text:a,__namedExportsOrder:s,default:o},Symbol.toStringTag,{value:"Module"}));export{i as C,e as D};
