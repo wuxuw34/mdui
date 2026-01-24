1dbb232cd3da5d0d738597c96fb5b3e7c33bf0e7
lijianfeng <ljfwbd@gmail.com>
2026-01-24 19:37:06 +0800

    创建tooltips

---
+++ ui/src/components/tooltips/index.ts
@@ -0,0 +1,3 @@
+
+export { default as MTooltips } from './tooltips'
+export * from './interface'
---
+++ ui/src/components/tooltips/tooltips.tsx
@@ -0,0 +1,7 @@
+export default function MTooltips() {
+  return (
+    <div>
+      <div className="mdui-tooltip">提示</div>
+    </div>
+  );
+}
